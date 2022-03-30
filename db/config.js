293d09b2793585708c5c062a520f116fe44a605d
kst <zyzuzizozz@naver.com>
2022-03-30 12:01:22 +0900

    노드에mysql데이터가져오기

---
+++ db/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    connectionLimit: 66,  
+    host : 'myfirstdb-1.cqzp0xslvjuo.us-east-1.rds.amazonaws.com',
+    user : 'admin',
+    password : 'zyzuzizozp1!',
+    port : '3306',
+    database : 'Portpolio'
+}
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var app = require('express')();
 
 // var postsend = require('./api/postsend')
 var getsend = require('./api/getsend')
-// var sqlreqres = require('./api/introduce')
+var sqlreqres = require('./api/introduce')
 // var preinterview = require('./api/preinterview')
 
 
 // app.use('/postsend', postsend); 
 app.use('/getsend', getsend);
-// app.use('/introduce', sqlreqres);
+app.use('/introduce', sqlreqres);
 // app.use('/prointerview', preinterview);
 
 app.set('port',8080)
